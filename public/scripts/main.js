7b57cd160c4a8f1718c02c6747f9d7f84553ff44
Sami Jaatinen <sami.jaatinen@iki.fi>
2014-04-06 22:07:30 +0300

    Kinetic.js added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,11 @@
 require.config({
     paths: {
         jquery: 'libs/jquery-2.1.0',
-        underscore: 'libs/underscore'
+        underscore: 'libs/underscore',
+        kinetic: 'libs/kinetic-v5.1.0'
     }
 })
 
 require(['moomoocards'], function (Cards) {
-    Cards.initialize("#mooAppContent")
+    Cards.initialize("mooAppContent")
 })
--- public/scripts/moomoocards.js
+++ public/scripts/moomoocards.js
@@ -1,16 +1,26 @@
-define(['jquery', 'underscore'], function () {
+define(['kinetic','jquery', 'underscore'], function () {
     var INITIAL_CARD = "/images/card_apina.png"
+
     var initialize = function (container) {
-        $(container).html("<canvas class='moomooCards'></canvas>")
-        var canvas = $(container + " .moomooCards")[0]
-        var context = canvas.getContext('2d')
-        var img = new Image()
-        img.src = INITIAL_CARD
-        img.onload = function () {
-            canvas.width = this.width
-            canvas.height = this.height
-            context.drawImage(this, 0, 0)
+        var stage = createStage(container)
+
+        var backgroundLayer = new Kinetic.Layer()
+        var background = new Image()
+        background.src= INITIAL_CARD
+        background.onload = function() {
+            backgroundLayer.add(new Kinetic.Image({image: background}))
+            stage.add(backgroundLayer)
         }
+    }
+
+    function createStage(container) {
+        var stage = new Kinetic.Stage({
+            container : container,
+            width : 640,
+            height : 420
+        })
+
+        return stage
     }
 
     return {
