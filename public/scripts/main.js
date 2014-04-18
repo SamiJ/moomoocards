df7b4455117e6e71a36a4a5d30802177b3ad37c9
Sami Jaatinen <sami.jaatinen@iki.fi>
2014-04-18 16:29:24 +0300

    Text layer

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,14 @@
 require.config({
     paths: {
         jquery: 'libs/jquery-2.1.0',
-        underscore: 'libs/underscore',
-        kinetic: 'libs/kinetic-v5.1.0'
+        _: 'libs/underscore',
+        kinetic: 'libs/kinetic-v5.1.0',
+        mooMooCards: 'moomoocards'
+    },
+    shim: {
+        kinetic: {exports: "Kinetic"}
     }
+
 })
 
 require(['moomoocards'], function (Cards) {
--- public/scripts/moomoocards.js
+++ public/scripts/moomoocards.js
@@ -1,8 +1,28 @@
-define(['kinetic','jquery', 'underscore'], function () {
+define(function(require) {
+    var $ = require('jquery')
+    var _ = require('_')
+    require('kinetic')
+
     var INITIAL_CARD = "/images/card_apina.png"
 
     var initialize = function (container) {
         var stage = createStage(container)
+        var message = $('#' + container).append('<textarea></textarea>')
+
+        var messageLayer = new Kinetic.Layer({draggable: true})
+        var message = new Kinetic.Text({
+            x: 100,
+            y: 60,
+            text: 'COMPLEX TEXT\n\nAll the world\'s a stage, and all the men and women merely players. They have their exits and their entrances.',
+            fontSize: 18,
+            fontFamily: 'Calibri',
+            fill: '#555',
+            width: 380,
+            padding: 20,
+            align: 'center'
+        })
+
+        messageLayer.add(message)
 
         var backgroundLayer = new Kinetic.Layer()
         var background = new Image()
@@ -10,7 +30,10 @@ define(['kinetic','jquery', 'underscore'], function () {
         background.onload = function() {
             backgroundLayer.add(new Kinetic.Image({image: background}))
             stage.add(backgroundLayer)
+            stage.add(messageLayer)
+            stage.draw()
         }
+
     }
 
     function createStage(container) {
