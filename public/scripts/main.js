bc574597b293ba2fd9fb702064ccaa756c9fdc43
Sami Jaatinen <sami.jaatinen@iki.fi>
2014-05-02 19:22:08 +0300

    remove unnecessary import

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,13 +2,11 @@ require.config({
     paths: {
         jquery: 'libs/jquery-2.1.0',
         _: 'libs/underscore',
-        kinetic: 'libs/kinetic-v5.1.0',
-        mooMooCards: 'moomoocards'
+        kinetic: 'libs/kinetic-v5.1.0'
     },
     shim: {
         kinetic: {exports: "Kinetic"}
     }
-
 })
 
 require(['moomoocards'], function (cards) {
