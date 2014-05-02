3c691a3a1057ee974db6152e30d372dd582ae255
Sami Jaatinen <sami.jaatinen@iki.fi>
2014-05-02 20:12:40 +0300

    tidy up the code

--- public/scripts/moomoocards.js
+++ public/scripts/moomoocards.js
@@ -4,69 +4,26 @@ define(function (require) {
     require('kinetic')
 
     var INITIAL_CARD = "/images/card_apina.png"
-    var dragLimitX = 0
-    var dragLimitY = 0
 
-    function updateDragLimits(image, message) {
-        dragLimitX = image.width - message.getTextWidth()
-        dragLimitY = image.height - message.getHeight()
-    }
+    var stage,
+        $container,
+        $message,
+        dragLimitX, dragLimitY,
+        backgroundLayer = new Kinetic.Layer(),
+        background = new Image()
 
     var initialize = function (container) {
-        var stage = createStage(container)
-        var $container = $('#' + container)
+        createInitialPage(container)
+        bindEvents()
+    }
+
+    function createInitialPage(container) {
+        stage = createStage(container)
+        $container = $('#' + container)
         $container.prepend('<textarea></textarea><br>')
         $container.append('<div id="js-save">Click here to save and share your card :)</div>')
-
-        var $message = $('#' + container + ">textarea")
-
-        var messageLayer = new Kinetic.Layer({
-            draggable: true,
-            dragBoundFunc: function (pos) {
-                return {
-                    x: Math.max(0, Math.min(dragLimitX, pos.x)),
-                    y: Math.max(0, Math.min(dragLimitY, pos.y))
-                }
-            }
-        })
-        var message = new Kinetic.Text({
-            x: 0,
-            y: 0,
-            fontSize: 24,
-            fontFamily: 'Calibri',
-            fill: '#fff',
-            width: 380,
-            padding: 0,
-            fontStyle: 'bold'
-        })
-
-        $message.on('input propertychange', function () {
-            message.text($message.val())
-            updateDragLimits(background, message)
-            messageLayer.draw()
-        })
-
-        $('#js-save', '#' + container).on('click', function () {
-            stage.toDataURL({
-                callback: function (dataUrl) {
-                    save($message.val(), dataUrl, function (id) {
-                        var cardUrl = window.location.origin + "/card/" + id
-                        var facebookShareUrl = "http://www.facebook.com/sharer.php?u=" + encodeURIComponent(cardUrl)
-                        //FIXME - templates for js rendering?
-                        $container.append('<div>')
-                        $container.append('Card created successfully!<br>')
-                        $container.append('<a target="_blank" href="' + cardUrl + '">' + cardUrl + '</a><br><br>')
-                        $container.append('Share it on <a href="' + facebookShareUrl + '" target="_blank">Facebook</a>')
-                        $container.append('</div>')
-                    })
-                }
-            })
-        })
-
+        $message = $('#' + container + ">textarea")
         messageLayer.add(message)
-
-        var backgroundLayer = new Kinetic.Layer()
-        var background = new Image()
         background.src = INITIAL_CARD
         background.onload = function () {
             updateDragLimits(background, message)
@@ -75,6 +32,60 @@ define(function (require) {
             stage.add(messageLayer)
             stage.draw()
         }
+    }
+
+    var message = new Kinetic.Text({
+        x: 0,
+        y: 0,
+        fontSize: 24,
+        fontFamily: 'Calibri',
+        fill: '#fff',
+        width: 380,
+        padding: 0,
+        fontStyle: 'bold'
+    })
+
+    var messageLayer = new Kinetic.Layer({
+        draggable: true,
+        dragBoundFunc: function (pos) {
+            return {
+                x: Math.max(0, Math.min(dragLimitX, pos.x)),
+                y: Math.max(0, Math.min(dragLimitY, pos.y))
+            }
+        }
+    })
+
+    function updateDragLimits(image, message) {
+        dragLimitX = image.width - message.getTextWidth()
+        dragLimitY = image.height - message.getHeight()
+    }
+
+
+    function bindEvents() {
+        $container.find('#js-save').on('click', function () {
+            stage.toDataURL({
+                callback: saveCardToDb
+            })
+
+            function saveCardToDb(dataUrl) {
+                save($message.val(), dataUrl, function (id) {
+                    var cardUrl = window.location.origin + "/card/" + id
+                    var facebookShareUrl = "http://www.facebook.com/sharer.php?u=" + encodeURIComponent(cardUrl)
+                    //FIXME - templates for js rendering?
+                    $container.append('<div>')
+                    $container.append('Card created successfully!<br>')
+                    $container.append('<a target="_blank" href="' + cardUrl + '">' + cardUrl + '</a><br><br>')
+                    $container.append('Share it on <a href="' + facebookShareUrl + '" target="_blank">Facebook</a>')
+                    $container.append('</div>')
+                })
+            }
+        })
+
+        $message.on('input propertychange', function () {
+            message.text($message.val())
+            updateDragLimits(background, message)
+            messageLayer.draw()
+        })
     }
 
     function createStage(container) {
