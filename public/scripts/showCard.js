af9239e762a90937126a25af8b14874106e0c097
Sami Jaatinen <sami.jaatinen@iki.fi>
2014-05-07 19:59:41 +0300

    simple error handling for unknown cardId

--- public/scripts/showCard.js
+++ public/scripts/showCard.js
@@ -1,7 +1,13 @@
-require(['dependencies'], function() {
-    require(['jquery'], function($) {
-            card = new Image()
-            card.src = "/api/card/" + cardId
-            $('#cardContent').append(card)
-        })
+require(['dependencies'], function () {
+    require(['jquery'], function ($) {
+        var container$ = $('#cardContent')
+        card = new Image()
+        card.onerror = function () {
+            container$.append("Sorry, your card seems to have been lost in the mail :(")
+        }
+        card.onload = function () {
+            container$.append(card)
+        }
+        card.src = "/api/card/" + cardId
+    })
 })
